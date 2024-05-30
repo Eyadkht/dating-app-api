e203129b3d6bf962674f84140af257cbbdb1aa7b
eyad <eyad.kht@gmail.com>
2024-05-30 23:24:46 +0100

    Update UserLogin and hash the password

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
 	gorm.io/driver/mysql v1.5.6 // indirect
 	gorm.io/gorm v1.25.10 // indirect
 )
--- pkg/handlers/user.go
+++ pkg/handlers/user.go
@@ -9,6 +9,8 @@ import (
 	"muzz-dating/pkg/models"
 
 	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
@@ -17,8 +19,8 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
-		error_response := map[string]string{"error": fmt.Sprintf("Method not allowed: %s", r.Method)}
-		json.NewEncoder(w).Encode(error_response)
+		errorResponse := map[string]string{"error": fmt.Sprintf("Method not allowed: %s", r.Method)}
+		json.NewEncoder(w).Encode(errorResponse)
 		return
 	}
 
@@ -27,10 +29,20 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	// TODO: Add field validation
 	if err := decoder.Decode(&newUser); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		error_response := map[string]string{"error": fmt.Sprintf("Error decoding request body: %v", err)}
-		json.NewEncoder(w).Encode(error_response)
+		errorResponse := map[string]string{"error": fmt.Sprintf("Error decoding request body: %v", err)}
+		json.NewEncoder(w).Encode(errorResponse)
 		return
 	}
+
+	// Hash the password before saving to the database
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		errorResponse := map[string]string{"error": fmt.Sprintf("Error hashing password: %v", err)}
+		json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+	newUser.Password = string(hashedPassword)
 
 	result := core.GetDb().Create(&newUser)
 	if err := result.Error; err != nil {
@@ -39,15 +51,15 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 			if mysqlErr.Number == 1062 {
 				// Handle duplicate entry
 				w.WriteHeader(http.StatusConflict)
-				error_response := map[string]string{"error": fmt.Sprintf("User with this email address already exists: %v", newUser.Email)}
-				json.NewEncoder(w).Encode(error_response)
+				errorResponse := map[string]string{"error": fmt.Sprintf("User with this email address already exists: %v", newUser.Email)}
+				json.NewEncoder(w).Encode(errorResponse)
 				return
 			}
 		} else {
 			// Handle other potential errors
 			w.WriteHeader(http.StatusBadRequest)
-			error_response := map[string]string{"error": fmt.Sprintf("Error creating user: %v", err)}
-			json.NewEncoder(w).Encode(error_response)
+			errorResponse := map[string]string{"error": fmt.Sprintf("Error creating user: %v", err)}
+			json.NewEncoder(w).Encode(errorResponse)
 			return
 		}
 	}
@@ -56,8 +68,8 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	encoder := json.NewEncoder(w)
 	if err := encoder.Encode(newUser); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		error_response := map[string]string{"error": fmt.Sprintf("Error serializing user to JSON: %v", err)}
-		json.NewEncoder(w).Encode(error_response)
+		errorResponse := map[string]string{"error": fmt.Sprintf("Error serializing user to JSON: %v", err)}
+		json.NewEncoder(w).Encode(errorResponse)
 		return
 	}
 }
@@ -68,7 +80,43 @@ func UserLogin(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
-		error_response := map[string]string{"error": fmt.Sprintf("Method not allowed: %s", r.Method)}
+		errorResponse := map[string]string{"error": fmt.Sprintf("Method not allowed: %s", r.Method)}
+		json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
+	var loginPayload struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&loginPayload); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		errorResponse := map[string]string{"error": fmt.Sprintf("Error decoding request body: %v", err)}
+		json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
+	var user models.User
+	result := core.GetDb().Where("email = ?", loginPayload.Email).First(&user)
+	if result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			w.WriteHeader(http.StatusNotFound)
+			error_response := map[string]string{"error": "Invalid credentials"}
+			json.NewEncoder(w).Encode(error_response)
+			return
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			error_response := map[string]string{"error": fmt.Sprintf("Error retrieving user: %v", result.Error)}
+			json.NewEncoder(w).Encode(error_response)
+			return
+		}
+	}
+
+	// Compare the hashed password stored in the database with the hash of the provided password
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginPayload.Password)); err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		error_response := map[string]string{"error": "Invalid credentials"}
 		json.NewEncoder(w).Encode(error_response)
 		return
 	}
@@ -83,8 +131,8 @@ func UserLogin(w http.ResponseWriter, r *http.Request) {
 
 	if err := encoder.Encode(response); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		error_response := map[string]string{"error": fmt.Sprintf("Error serializing user to JSON: %v", err)}
-		json.NewEncoder(w).Encode(error_response)
+		errorResponse := map[string]string{"error": fmt.Sprintf("Error serializing user to JSON: %v", err)}
+		json.NewEncoder(w).Encode(errorResponse)
 		return
 	}
 }
